bc5566361193cc05e372685089c726b1ebad8dad
sahayapoorv <sahay.apoorva@gmail.com>
2016-08-28 23:44:48 -0500

    Added implementation for the Delete Node Problem.

--- DeleteNode/Source.cpp
+++ DeleteNode/Source.cpp
@@ -13,17 +13,81 @@ class Solution {
 public:
 	void deleteNode(ListNode* node) {
 		ListNode* temp = node;
-		while (temp)
+		ListNode* temp2 = NULL;
+		if (!node->next)
 		{
-			if (temp->val == 3 && temp->next)
-			{
-
-			}
+			return;
 		}
+		else
+		{
+			while (temp->next)
+			{
+				temp->val = temp->next->val;
+				
+				if (!temp->next->next)
+				{
+					temp2 = temp->next;
+					temp->next = NULL;
+				}
+				else 
+				{
+					temp = temp->next;
+				}
+			}
+
+			delete temp2;
+		}
+	}
+
+	void addNode(ListNode* node, int a)
+	{
+		if (!node)
+		{
+			node = new ListNode(a);
+		}
+		else
+		{
+			ListNode* temp = node;
+			ListNode* newNode = new ListNode(a);
+			while (temp)
+			{
+				if (!temp->next)
+				{
+					break;
+				}
+
+				temp = temp->next;
+			}
+
+			temp->next = newNode;
+		}
+	}
+
+	void DeleteLL(ListNode* node)
+	{
+		ListNode* temp = node;
+
+		while (temp->next)
+		{
+			node = temp->next;
+			delete temp;
+			temp = node;
+		}
+
+		delete node;
 	}
 };
 
 int main()
 {
+	ListNode* node = new ListNode(1);
+	Solution s;
+	s.addNode(node, 2);
+	s.addNode(node, 3);
+	s.addNode(node, 4);
+
+	s.deleteNode(node);
+	s.DeleteLL(node);
+
 	return 0;
 }
